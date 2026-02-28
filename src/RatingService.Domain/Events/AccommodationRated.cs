06847134154924da0fa2fdbcae2dd825d7755104
Ilija Spasic <spasicilija1@gmail.com>
2026-02-28 22:52:59 +0100

    fix: fix events namespace and types

--- src/RatingService.Domain/Events/AccommodationRated.cs
+++ src/RatingService.Domain/Events/AccommodationRated.cs
@@ -1,4 +1,4 @@
-namespace RatingService.Domain;
+namespace Hotelier.Events;
 
 /// <summary>
 /// Published when a guest rates an accommodation.
@@ -10,6 +10,8 @@ public record AccommodationRated
     public Guid RatingId { get; init; }
     public Guid GuestId { get; init; }
     public Guid AccommodationId { get; init; }
+    public Guid HostId { get; init; }
     public int Score { get; init; }
+    public string? Comment { get; init; }
     public DateTime Timestamp { get; init; } = DateTime.UtcNow;
 }
