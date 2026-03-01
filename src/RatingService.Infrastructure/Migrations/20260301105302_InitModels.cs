1931dd6d85b4b410c9714ede0e722ac894134338
Ilija Spasic <spasicilija1@gmail.com>
2026-03-01 11:54:28 +0100

    chore: add init migration

---
+++ src/RatingService.Infrastructure/Migrations/20260301105302_InitModels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RatingService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    GuestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TargetId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TargetType = table.Column<int>(type: "integer", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_GuestId",
+                table: "Ratings",
+                column: "GuestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_GuestId_TargetId_TargetType",
+                table: "Ratings",
+                columns: new[] { "GuestId", "TargetId", "TargetType" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_TargetId_TargetType",
+                table: "Ratings",
+                columns: new[] { "TargetId", "TargetType" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
